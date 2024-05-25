29d792c3a016739e6f6ee85acb5af3b95d8235ed
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-05-25 15:14:26 -0600

    chore: Update Hero component title
    This commit updates the title of the Hero component in the src/components/Hero.tsx file. The previous title "UKx Energy Mix" has been changed to "UK Energy Mix". This change aims to provide a more accurate and concise description of the app's purpose.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { formatDate } from "../utils/dataFormat";
 export const Hero = ({ data }: { data: EnergyData }) => {
   return (
     <section className="Hero">
-      <h1 className="Hero-title">UKx Energy Mix</h1>
+      <h1 className="Hero-title">UK Energy Mix</h1>
       <p className="Hero-subtitle">
         This is a simple app to show the UK energy mix
       </p>
